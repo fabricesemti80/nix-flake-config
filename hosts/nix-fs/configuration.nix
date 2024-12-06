09ace9d1ade025b0dd08b7d0673f616abd7e29cf
Fabrice Semti <37410363+fabricesemti80@users.noreply.github.com>
2024-12-06 08:18:33 +0000

    adjust vscode server fix

--- hosts/nix-fs/configuration.nix
+++ hosts/nix-fs/configuration.nix
@@ -36,11 +36,10 @@
       pathConfig.PathChanged = "%h/.vscode-server/bin";
     };
     services.vscode-remote-workaround.script = ''
-      #!/bin/sh
       for i in ~/.vscode-server/bin/*; do
-        if [ -e "$i/node" ]; then
+        if [ -e $i/node ]; then
           echo "Fixing vscode-server in $i..."
-          ln -sf ${pkgs.nodejs_18}/bin/node "$i/node"
+          ln -sf ${pkgs.nodejs_18}/bin/node $i/node
         fi
       done
     '';
